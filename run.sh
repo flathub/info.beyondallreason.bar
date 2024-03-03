a22d422efb59db41340d23a2d075993c672096b3
Marek Rusinowski <marekrusinowski@gmail.com>
2024-03-03 14:05:45 +0100

    Update launcher to the latest version
    As part of the update, I had to refactor quite a bit of how the build
    process works.
    
    flatpak-node-generator is quite bugged:
     - Output `generated-source.json` expects the package to be in the main
       directory, so I've split the build into modules but still had to add
       `flatpak-node` exclusion to build
     - I had to patch locally https://github.com/flatpak/flatpak-builder-tools/pull/382
       and use that because https://github.com/flatpak/flatpak-builder-tools/issues/381
     - I've also run into https://github.com/flatpak/flatpak-builder-tools/issues/377
    
    I've also:
     - Made copying of addr2line dependencies more reliable
     - Merged the startup scripts into one
     - Added permission and setup for discord so that rich presence works

---
+++ run.sh
@@ -0,0 +1,6 @@
+# https://github.com/flathub/com.discordapp.Discord/wiki/Rich-Precense-(discord-rpc)
+for i in {0..9}; do
+    test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
+done
+
+zypak-wrapper.sh /app/main/beyond-all-reason --disable-launcher-update -w $XDG_DATA_HOME "$@"
