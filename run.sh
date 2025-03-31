dc0d3ed1275b5248f199d4ced56c4cff505d6c49
xlcdev <119954023+xlc-dev@users.noreply.github.com>
2025-03-31 13:17:12 +0200

    fix bwrap: execvp run.sh: Exec format error (#48)

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # https://github.com/flathub/com.discordapp.Discord/wiki/Rich-Precense-(discord-rpc)
 for i in {0..9}; do
     test -S $XDG_RUNTIME_DIR/discord-ipc-$i || ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-$i;
